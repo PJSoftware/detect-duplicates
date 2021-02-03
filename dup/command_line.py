6551abd1e4283b557b7b9fe2723d4bac1758c88f
Peter Jones <pjsoftware@petesplace.id.au>
2021-02-03 11:56:37 +1000

    add support for progress bar; reorganise argparse input

--- dup/command_line.py
+++ dup/command_line.py
@@ -11,28 +11,34 @@ def main():
     parser = argparse.ArgumentParser(
         description=f"dup v{version()}: find duplicate files within the current folder", 
         epilog="* not yet implemented")
-    parser.add_argument(
-        "-v", "--verbose", action="count", default=0,
-        help="increase output verbosity (up to 3 levels)")
-    parser.add_argument("-z", "--zero", action="store_true", help="include zero-length files")
-    parser.add_argument("-x", "--hidden", action="store_true", help="include hidden files/folders")
-    parser.add_argument("-r", "--rehearse", action="store_true", help="show intended actions without doing anything*")
-    
-    behave = parser.add_argument_group("behaviour")
-    behave.add_argument("--find", action="store_true", help="report duplicates only (default)")
-    behave.add_argument("--move", action="store_true", help="move duplicates into separate folder for review*")
-    behave.add_argument("--delete", action="store_true", help="delete duplicates*")
-    
-    other = parser.add_argument_group("extensions")
-    other.add_argument("--list-ext", action="store_true", help="list unique extensions in folder tree")
-    other.add_argument("-a", "--all", action="store_true", help="list all extensions (default is top 10 only)")
-    other.add_argument("--show-ext", help="list files with specified extension (eg, --show-ext JPG)")
-    other.add_argument("--del-ext", help="delete files with specified extension (eg, --del-ext MD)*")
-    other.add_argument("-i", "--ignore-case", action="store_true", help="ignore case when specifying extension")
+
+    p_act = parser.add_argument_group("actions")
+    p_act.add_argument("--find", action="store_true", help="report duplicates only (default)")
+    p_act.add_argument("--move", action="store_true", help="move duplicates into separate folder for review*")
+    p_act.add_argument("--delete", action="store_true", help="delete duplicates*")
+
+    p_ext = parser.add_argument_group("extensions")
+    p_ext.add_argument("--list-ext", action="store_true", help="list unique extensions in folder tree")
+    p_ext.add_argument("-a", "--all", action="store_true", help="list all extensions (default is top 10 only)")
+    p_ext.add_argument("--show-ext", help="list files with specified extension (eg, --show-ext JPG)")
+    p_ext.add_argument("--del-ext", help="delete files with specified extension (eg, --del-ext MD)*")
+    p_ext.add_argument("-i", "--ignore-case", action="store_true", help="ignore case when specifying extension")
+
+    p_opt = parser.add_argument_group("options")
+    p_opt.add_argument("-z", "--zero", action="store_true", help="include zero-length files")
+    p_opt.add_argument("-x", "--hidden", action="store_true", help="include hidden files/folders")
+    p_opt.add_argument("-r", "--rehearse", action="store_true", help="show intended actions without doing anything*")
+
+    p_stat = parser.add_argument_group("status")
+    p_stat.add_argument("-p", "--progress", action="store_true", help="display progress bar (default)*")
+    p_stat.add_argument(
+        "-v", "--verbose", action="count", default=Verbosity.Required, 
+        help="increase output verbosity, up to 3 levels (disables --progress)")
 
     arg = parser.parse_args()
 
     output(f"dup v{version()}")
+    config.PROGRESS_BAR = True
     config.VERBOSITY_LEVEL = min(arg.verbose, Verbosity.Waffle)
     config.INCLUDE_HIDDEN = arg.hidden
     config.IGNORE_ZERO_LENGTH = not arg.zero
@@ -42,15 +48,18 @@ def main():
     if arg.rehearse:
         config.SHOW_DONT_ACT = True
         config.VERBOSITY_LEVEL = max(config.VERBOSITY_LEVEL, Verbosity.Detailed)
-        output("* --show specified; no action will be taken (implies -vv)", Verbosity.Detailed)
+        output("* --rehearse specified; no action will be taken (implies -vv)", Verbosity.Required)
 
-    output(f"* verbosity level set to {config.VERBOSITY_LEVEL}", Verbosity.Detailed)
+    if config.VERBOSITY_LEVEL > Verbosity.Required:
+        config.PROGRESS_BAR = False
+        output(f"* verbosity level set to {config.VERBOSITY_LEVEL}", Verbosity.Detailed)
+        output("* progress bar has been disabled", Verbosity.Detailed)
     
     if arg.zero:
-        output("* --zero specified; zero-length files will be included in comparison", Verbosity.Detailed)
+        output("* --zero specified; zero-length files will be included in comparison", Verbosity.Required)
     
     if arg.hidden:
-        output("* --hidden specified; hidden files and folders will be included", Verbosity.Detailed)
+        output("* --hidden specified; hidden files and folders will be included", Verbosity.Required)
     
     # process extension commands first
     if arg.del_ext:
