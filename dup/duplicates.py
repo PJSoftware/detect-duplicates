99b80cd34439bff8536e3b3b29589226abe0b830
Peter Jones <pjsoftware@petesplace.id.au>
2021-02-03 15:45:38 +1000

    improve reporting of duplicate files

--- dup/duplicates.py
+++ dup/duplicates.py
@@ -3,7 +3,7 @@ import os
 
 from . import recurse_into_folder, output, plural, foldername
 from .config import Verbosity
-from . import global_var
+from . import global_var, config
 
 def find():
     output("Find duplicates", Verbosity.Required)
@@ -60,31 +60,41 @@ def hash_file(file_path: str) -> str:
 def report_duplicates(by_hash: dict):
     dup = plural(global_var.duplicates_found, "duplicate file")
     output(f"> {dup} found", Verbosity.Required)
-    by_count = {}
-    for size in by_hash:
-        output(f"size: {size}", Verbosity.Waffle)
-        for hash in by_hash[size]:
-            output(f"hash: {hash}", Verbosity.Waffle)
-            count = len(by_hash[size][hash])
-            output(f"count: {count}", Verbosity.Waffle)
+    if config.VERBOSITY_LEVEL == Verbosity.Required:
+        by_count = {}
+        for size in by_hash:
+            output(f"size: {size}", Verbosity.Waffle)
+            for hash in by_hash[size]:
+                output(f"hash: {hash}", Verbosity.Waffle)
+                count = len(by_hash[size][hash])
+                output(f"count: {count}", Verbosity.Waffle)
 
-            if count > 1:
-                if not count in by_count:
-                    by_count[count] = {}
-                if not size in by_count[count]:
-                    by_count[count][size] = 0
-                by_count[count][size] += 1
-    for count in (sorted(by_count.keys(), reverse=True)):
-        num = 0
-        min_size = -1
-        max_size = -1
-        for size in (sorted(by_count[count])):
-            num += by_count[count][size]
-            if min_size == -1:
-                min_size = size
-            max_size = max(max_size, size)
-        sets = plural(num, "set")
-        size_range = f"{min_size}"
-        if max_size > min_size:
-            size_range += f"..{max_size}"
-        output(f"> {sets} of files with {count} duplicates ({size_range})", Verbosity.Required)
+                if count > 1:
+                    if not count in by_count:
+                        by_count[count] = {}
+                    if not size in by_count[count]:
+                        by_count[count][size] = []
+                    by_count[count][size] = by_hash[size][hash]
+
+        for count in (sorted(by_count.keys(), reverse=True)):
+            num = 0
+            min_size = -1
+            max_size = -1
+            for size in (sorted(by_count[count])):
+                num += len(by_count[count][size])
+                if min_size == -1:
+                    min_size = size
+                max_size = max(max_size, size)
+            sets = plural(num, "set")
+            size_range = f"{min_size}"
+            if max_size > min_size:
+                size_range += f"..{max_size}"
+            output(f"> {sets} of files with {count} duplicates ({size_range})", Verbosity.Required)
+    else:
+        for size in (sorted(by_hash.keys())):
+            for hash in by_hash[size]:
+                count = len(by_hash[size][hash])
+                if count > 1:
+                    output(f"> {size}-byte files with {count} duplicates", Verbosity.Required)
+                    for file in (sorted(by_hash[size][hash])):
+                        output(f"> {file}", Verbosity.Information)
