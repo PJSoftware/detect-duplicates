f1154c545acc80e229a0f7744ac43f462538fe40
Peter Jones <pjsoftware@petesplace.id.au>
2021-02-05 04:09:31 +1000

    threshold=0 activates activity indicator

--- dup/progress.py
+++ dup/progress.py
@@ -6,6 +6,7 @@ class Bar:
     width = 40
     status = 0
     threshold = 100
+    step = 0
 
     def __init__(self, prefix: str, width: int, threshold: int, suffix: str = ""):
         self.prefix = prefix
@@ -13,19 +14,34 @@ class Bar:
         self.threshold = threshold
         self.suffix = suffix
         self.status = 0
+        self.step = 0
 
     def __repr__(self):
         if self.status > self.threshold:
             self.threshold = self.status
-        bar_done = int(self.width * self.status/self.threshold)
-        pc = int(1000 * self.status/self.threshold) / 10
-        bar_remain = self.width - bar_done
-        bar = "#"*bar_done + " "*bar_remain
-        return f"\r{self.prefix}: [{bar}] {pc}% {self.suffix}"
+        if self.threshold > 0:
+            bar_done = int(self.width * self.status/self.threshold)
+            pc = int(1000 * self.status/self.threshold) / 10
+            bar_remain = self.width - bar_done
+            bar = "#"*bar_done + " "*bar_remain
+            return f"\r{self.prefix}: [{bar}] {pc}% {self.suffix}"
+        else:
+            self.step += 1
+            if self.step > self.width:
+                pos = self.width*2 - self.step
+            else:
+                pos = self.step
+            bar_l = " " * (pos-1)
+            bar_r = " " * (self.width - pos)
+            bar = bar_l + "#" + bar_r
+            if self.step == (self.width-1)*2:
+                self.step = 0
+            return f"\r{self.prefix}: [{bar}] {self.suffix}"
 
-    def update(self, status: int, suffix: str = ""):
+    def update(self, status: int = 0, suffix: str = ""):
         if suffix != "":
             self.suffix = suffix
-        self.status = status
+        if status > self.status:
+            self.status = status
         bar = self.__repr__()
         print(f"\r{bar}", end="")
--- tests/test_code/progress_bar.py
+++ tests/test_code/progress_bar.py
@@ -8,8 +8,15 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../.
 from dup.progress import Bar
 # pylint: enable=no-name-in-module
 
-length = 100
-pb = Bar("Computing", 40, length)
+length = 175
+pb1 = Bar("Computing", 40, length)
 for i in range(length+1):
-    pb.update(i, f"Step {i}")
+    pb1.update(i, f"Step {i}")
     time.sleep(0.1) # any calculation you need
+print()
+
+pb2 = Bar("Scanning", 40, 0)
+for i in range(length+1):
+    pb2.update(0, f"Step {i}")
+    time.sleep(0.1) # any calculation you need
+print()
